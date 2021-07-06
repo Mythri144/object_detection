b51e4e5dda04b73da00bfe836a0d7d7a4df2982d
Mythri144 <72304576+Mythri144@users.noreply.github.com>
2021-07-06 14:59:57 +0530

    Add files via upload

---
+++ livingRoom.js
@@ -0,0 +1,64 @@
+img = "";
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage("livingRoom.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function back(){
+    window.location = "home.html";
+}
+
+function draw(){
+    image(img, 0, 0, 600, 500);
+    // fill("#FF0000");
+    // text("Dog", 100, 80);
+    // noFill();
+    // stroke("#FF0000");
+    // rect(90, 60, 450, 380);
+
+    // fill("#FF0000");
+    // text("Cat", 310, 85);
+    // noFill();
+    // stroke("#FF0000");
+    // rect(300, 65, 300, 380);
+
+    if(status != ""){
+        detector.detect(img, gotResults);
+
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("no_objects").innerHTML = "The Number Of Objects Detected Are = " + objects.length;
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#000000");
+            percent = (objects[i].confidence * 100).toFixed(0);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+
+            stroke("#000000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Loaded! ");
+    status = true;
+
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        objects = results;
+    }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function navDesk(){
+    window.location = "desk.html";
+}
+
+function navViewFromWindow(){
+    window.location = "viewFromWindow.html";
+}
+
+function navGarage(){
+    window.location = "garage.html";
+}
+
+function navBedroom(){
+    window.location = "bedroom.html";
+}
+
+function navLivingRoom(){
+    window.location = "livingRoom.html";
+}
